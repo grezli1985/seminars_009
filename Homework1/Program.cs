938a1faff6a598c44c0de91e69cafdfe41453d39
grezli <grezli@mail.ru>
2022-06-24 00:58:02 +0300

    Задача 2 Решена

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -13,15 +13,25 @@ Console.WriteLine("Задача 2.");
 
 Console.WriteLine("Введите первое число");
 int numberA = Convert.ToInt32(Console.ReadLine());
+//int numberA = 5;
 Console.WriteLine("Введите второе число");
 int numberB = Convert.ToInt32(Console.ReadLine());
-int max = number;
-if (numberA > numberB)
+//int numberB = 7;
+int max = 0;
+int min = 0;
+if (numberA > numberB) 
 {
     max = numberA;
-    Console.WriteLine("max = ");
-    Console.WrilteLine(max);
+    min = numberB;
 }
-
+else
+{
+    min = numberA;
+    max = numberB;
+}
+Console.Write("min = ");
+Console.WriteLine(min);
+Console.Write("max = ");
+Console.WriteLine(max);
 
 
