49d4be82c244b730a4620becf1c0e8c57d0a45d5
grezli <grezli@mail.ru>
2022-06-24 22:31:43 +0300

    Задача

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -6,4 +6,22 @@
 44 5 78 -> 78
 22 3 9 -> 22     */
 
+//int numberA = 2;
+//int numberB = 3;
+//int numberC = 7;
+int max = 0;
+
+Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int numberС = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB) max = numberA;
+if (numberB > numberС) max = numberB;
+if (numberС > numberA) max = numberС;
+
+Console.Write("max = ");
+Console.WriteLine(max);
 
---
+++ Homework3/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+является ли число чётным (делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет   */
+
+Console.WriteLine();
+Console.WriteLine("__________________");
+Console.WriteLine("Задача 6.");
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+OurPower(number);
+
+
+void OurPower(int currentNumber)
+{   
+    if(number%2 == 0)
+    {
+        Console.WriteLine($"{number} число чётное ");
+    }
+    else
+    {
+        Console.WriteLine($"{number} число нечётное ");
+    }
+}
+
+
+
