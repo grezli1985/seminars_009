6227a4eef3323666105aa415f39b1a90f271649d
grezli <grezli@mail.ru>
2022-06-25 11:29:14 +0300

    ?????

--- Homework_add_1/Program.cs
+++ Homework_add_1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 1. 
+Пользователь вводит число N (N > 0). 
+Программа должна вывести N единиц на экран.
+
+N = 4 -> 1, 1, 1, 1
+N = 2 -> 1, 1    */
+
+/*Console.WriteLine();
+Console.WriteLine("__________________");
+Console.WriteLine("Задача 1 доп.");
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine()); */
+
+int a = 5;
+int count = 0;
+int b = 0;
+if (count >= a)
+{
+    while (b = a / a);
+           a = a - 1;
+    count++;
+    Console.Write(b + " ");
+}
+
