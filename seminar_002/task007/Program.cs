93930b16eb54af67f03c8e5d79880af3d04914ef
grezli <grezli@mail.ru>
2022-06-27 21:45:35 +0300

    add seminar_002/task008/

---
+++ seminar_002/task007/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 
+11. Напишите программу,
+ которая выводит случайное трёхзначное число
+  и удаляет вторую цифру этого числа.*/
+// 456 -> 46
+/*
+Random rand = new Random();
+int number = rand.Next(100, 1000);
+Zadacha9(number);
+
+void Zadacha9(int currentNumber)
+{
+    Console.WriteLine(currentNumber);
+    //96 -> 96/10 = 9    96 % 6 = 6
+    int digitFirst = currentNumber/100;
+    int digisecond = currentNumber%10;
+    int result = digitFirst*10 + digisecond;
+    
+    
+    Console.WriteLine(result);
+}
+*/
+
+Random rand = new Random();
---
+++ seminar_002/task008/Program.cs
@@ -0,0 +1,25 @@
+﻿/*12. Напишите программу, 
+которая будет принимать на вход два числа и выводить, 
+является ли второе число кратным первому. 
+Если число 2 не кратно числу 1, 
+то программа выводит остаток от деления.*/
+
+Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Zadacha9(numberA, numberB);
+
+void Zadacha9(int arg1, int arg2)
+{
+    if (arg1%arg2 == 0)
+    {
+        Console.WriteLine("является");
+    }
+    else
+    {
+        Console.WriteLine("не является");
+    }
+}
+    
