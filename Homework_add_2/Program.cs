e94943cd0491759e8b141795fe68191fdc1fd910
grezli <grezli@mail.ru>
2022-06-27 18:07:12 +0300

    add Homework add 3 не доделал

--- Homework_add_2/Program.cs
+++ Homework_add_2/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 2.
+ Даны два числа a, b, такие что a < b.
+ Вывести на экран сколько раз число a поместиться в числе b. */
+
+/*Console.WriteLine();
+Console.WriteLine("__________________");
+Console.WriteLine("Задача 2 доп."); */
+
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+
+Div(result);
+
+void Div (int once)
+{
+if (a < b) ;
+{
+    int result = (b / a);
+    Console.WriteLine($"{result} раз столько раз поместиться первое число во второе!");
+}
+}
+
+
