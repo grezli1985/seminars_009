e143fbd90c97235c0c9b50736c56436a0aa0d3f9
grezli <grezli@mail.ru>
2022-06-27 21:03:43 +0300

    seminar 002

---
+++ seminar_002/task006/Program.cs
@@ -0,0 +1,26 @@
+﻿
+//Console.WriteLine("Hello, World!");
+
+Random rand = new Random();
+int number = rand.Next(10, 100);
+Zadacha9(number);
+
+void Zadacha9(int currentNumber)
+{
+    Console.WriteLine(currentNumber);
+    //96 -> 96/10 = 9    96 % 6 = 6
+    int digitFirst = currentNumber/10;
+    int digisecond = currentNumber%10;
+    if (digitFirst > digisecond)
+    {
+        Console.WriteLine(digisecond);
+    }
+    else if (digitFirst < digisecond)
+    {
+        Console.WriteLine(digisecond);
+    }
+    else
+    {
+        Console.WriteLine("ифры числа равны");
+    }
+}
