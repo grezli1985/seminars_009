ac79a1b5424b0792b11bb7d25fb0120d2bf64524
grezli <grezli@mail.ru>
2022-06-27 18:11:29 +0300

    Перенес в папку seminar 001

---
+++ seminar_001/Homework1/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Задача 2: Напишите программу, 
+которая на вход принимает два числа и выдаёт, 
+какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3    */
+
+Console.WriteLine();
+Console.WriteLine("__________________");
+Console.WriteLine("Задача 2.");
+
+
+Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+//int numberA = 5;
+//int numberB = 7;
+int max = 0;
+int min = 0;
+if (numberA > numberB) 
+{
+    max = numberA;
+    min = numberB;
+}
+else
+{
+    min = numberA;
+    max = numberB;
+}
+Console.Write("min = ");
+Console.WriteLine(min);
+Console.Write("max = ");
+Console.WriteLine(max);
---
+++ seminar_001/Homework_add_1/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Задача 1. 
+Пользователь вводит число N (N > 0). 
+Программа должна вывести N единиц на экран.
+
+N = 4 -> 1, 1, 1, 1
+N = 2 -> 1, 1    */
+
+/*Console.WriteLine();
+Console.WriteLine("__________________");
+Console.WriteLine("Задача 1 доп."); */
+
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+
+OneWarrior(N);
+
+void OneWarrior( int N)
+{
+int count = 0;
+if (N > 0)
+{
+    while (count < N)
+    {
+    Console.Write("1 ");    
+    count++;
+    }
+}
+}
+
+
