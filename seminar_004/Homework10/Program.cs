40f62a5ce865abb5123f6a8a5feb1c11fc6b9654
grezli <grezli@mail.ru>
2022-07-06 13:57:45 +0300

    add seminar 004 Homework 10

---
+++ seminar_004/Homework10/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход
+два натуральных числа (A и B) и возводит число A в
+степень B. 
+3, 5 -> 243 (3⁵)
+2, 4 -> 16         */ 
+
+void Zadacha25()
+{
+    
+Console.Write("Введите число A = ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B = ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"возводим число {numberA} в степень {numberB} = {Exponentiation(numberA, numberB)}");
+
+// Console.WriteLine($"возводим число  {numberA} в степень {numberB} равно {Math.Pow(numberA, numberB)}");
+
+}
+
+//3, 5 -> 243 (3⁵)        3           5
+int Exponentiation(int numberA, int numberB)
+{  
+        int result = 1; 
+        int i = numberB;
+        while (i > 0)
+        {
+            result = result * numberA;
+            i--;
+        }
+        return result;
+        
+}
+
+Zadacha25();
