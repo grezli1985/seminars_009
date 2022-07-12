36a5abd4ff3af35398cc5f03f860a9d1f3704174
grezli <grezli@mail.ru>
2022-07-12 23:06:27 +0300

    seminar 005 Homework 14

---
+++ seminar_005/Homework14/Program.cs
@@ -0,0 +1,53 @@
+﻿/* Задача 36:
+Задайте одномерный массив, заполненный случайными числами.
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0    */
+
+void Zadacha36()
+{
+    Random random = new Random();
+    int size = random.Next(4, 12);
+    int[] array = new int[size];
+    FillArray(array, 1, 10);
+    PrintArray(array);
+    Console.WriteLine(
+        "Сумму элементов, стоящих на нечётных позициях: " + TheSumOfElementsInOddPositions(array)
+    );
+}
+
+int TheSumOfElementsInOddPositions(int[] arr)
+{
+    int sum = 0;
+    int i = 1;
+    while (i < arr.Length)
+    {
+        sum += arr[i];
+        i += 2;
+    }
+    return sum;
+}
+
+void FillArray(int[] array, int startNumber = 0, int finishNumber = 0)
+{
+    finishNumber++;
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(startNumber, finishNumber);
+    }
+    Console.WriteLine();
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("Вывод массива");
+    Console.WriteLine();
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+Zadacha36();
