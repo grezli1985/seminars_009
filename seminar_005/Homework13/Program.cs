e62bd0a92b460e04b793736a8418a742a55c4b53
grezli <grezli@mail.ru>
2022-07-12 16:53:12 +0300

    seminar 005 Homework 13

---
+++ seminar_005/Homework13/Program.cs
@@ -0,0 +1,60 @@
+﻿/* Задача 34: Задайте массив заполненный случайными
+положительными трёхзначными числами. Напишите
+программу, которая покажет количество чётных чисел в
+массиве.
+[345, 897, 568, 234] -> 2  */
+
+void Zadacha34()
+{
+    Random random = new Random();
+    int size = random.Next(4, 8);
+    int[] array = new int[size];
+    FillArray(array, 100, 1000);
+    PrintArray(array);
+    NumberOfEvenNumbersInTheArray(array);
+
+}
+
+void NumberOfEvenNumbersInTheArray(int[] array)
+{
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            //Console.WriteLine($"{array[i]} число чётное ");
+            count++;
+        }
+        // else
+        // {
+        //     Console.WriteLine($"{array[i]} число нечётное ");
+        // }
+    }
+
+    Console.WriteLine(count + " - количество чётных чисел в массиве");
+}
+
+void FillArray(int[] array, int startNumber = 0, int finishNumber = 0)
+{
+    finishNumber++;
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(startNumber, finishNumber);
+    }
+    Console.WriteLine();
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("Вывод массива");
+    Console.WriteLine();
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+Zadacha34();
