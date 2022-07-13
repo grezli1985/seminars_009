ae985e369f2df0c64c7084ef06cf3a75b7f6d0a3
grezli <grezli@mail.ru>
2022-07-13 19:36:16 +0300

    add seminar 005

--- seminar_005/Homework13/Program.cs
+++ seminar_005/Homework13/Program.cs
@@ -4,38 +4,83 @@
 массиве.
 [345, 897, 568, 234] -> 2  */
 
+// void Zadacha34()
+// {
+//     Random random = new Random();
+//     int size = random.Next(4, 8);
+//     int[] array = new int[size];
+//     FillArray(array, 100, 1000);
+//     PrintArray(array);
+//     NumberOfEvenNumbersInTheArray(array);
+
+// }
+
+// void NumberOfEvenNumbersInTheArray(int[] array)
+// {
+//     int count = 0;
+
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] % 2 == 0)
+//         {
+//             //Console.WriteLine($"{array[i]} число чётное ");
+//             count++;
+//         }
+//         // else
+//         // {
+//         //     Console.WriteLine($"{array[i]} число нечётное ");
+//         // }
+//     }
+
+//     Console.WriteLine(count + " - количество чётных чисел в массиве");
+// }
+
+// void FillArray(int[] array, int startNumber = 0, int finishNumber = 0)
+// {
+//     finishNumber++;
+//     Random random = new Random();
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = random.Next(startNumber, finishNumber);
+//     }
+//     Console.WriteLine();
+// }
+
+// void PrintArray(int[] array)
+// {
+//     Console.Write("Вывод массива");
+//     Console.WriteLine();
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// Zadacha34();
+
+
+
+
 void Zadacha34()
 {
-    Random random = new Random();
-    int size = random.Next(4, 8);
-    int[] array = new int[size];
-    FillArray(array, 100, 1000);
+    int[] array = new int[8];
+    FillArray(array, 100, 999);
     PrintArray(array);
-    NumberOfEvenNumbersInTheArray(array);
-
+    CountEvenNumber(array);
 }
 
-void NumberOfEvenNumbersInTheArray(int[] array)
+void CountEvenNumber(int[] array)
 {
     int count = 0;
-
     for (int i = 0; i < array.Length; i++)
     {
-        if (array[i] % 2 == 0)
-        {
-            //Console.WriteLine($"{array[i]} число чётное ");
-            count++;
-        }
-        // else
-        // {
-        //     Console.WriteLine($"{array[i]} число нечётное ");
-        // }
+        if(array[i] % 2 == 0) count++;
     }
-
-    Console.WriteLine(count + " - количество чётных чисел в массиве");
+    Console.WriteLine("Количество четных чисел в массиве: " + count);
 }
 
-void FillArray(int[] array, int startNumber = 0, int finishNumber = 0)
+void FillArray(int[] array, int startNumber = 0, int finishNumber = 1)
 {
     finishNumber++;
     Random random = new Random();
@@ -48,13 +93,13 @@ void FillArray(int[] array, int startNumber = 0, int finishNumber = 0)
 
 void PrintArray(int[] array)
 {
-    Console.Write("Вывод массива");
-    Console.WriteLine();
+    Console.WriteLine("Вывод массива");
     for (int i = 0; i < array.Length; i++)
     {
-        Console.Write(array[i] + " ");
+        Console.Write(array[i] + "\t");
     }
     Console.WriteLine();
 }
+
 
 Zadacha34();
--- seminar_005/Homework15/Program.cs
+++ seminar_005/Homework15/Program.cs
@@ -1,55 +1,108 @@
-﻿/* Задача 38: 
-Задайте массив вещественных чисел. 
+﻿/* Задача 38:
+Задайте массив вещественных чисел.
 Найдите разницу между максимальным и минимальным элементов массива.
-[3 7 22 2 78] -> 76   */      
+[3 7 22 2 78] -> 76   */
+
+// void Zadacha38()
+// {
+//     Random random = new Random();
+//     int size = random.Next(4, 12);
+//     double[] array = new double[size];
+//     FillArray(array, -1000000000000000000, 10000000000000000000);
+//     PrintArray(array);
+//     DifferenceBetweenMaxAndMinArrayElements(array);
+
+// }
+
+
+// void DifferenceBetweenMaxAndMinArrayElements(double[] arr)
+// {
+//     int min = 0;
+//     int max = 0;
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         if (arr[i] > arr[max]) max = i;
+//         if (arr[i] < arr[min]) min = i;
+
+//     }
+//     Console.WriteLine($"от max = {arr[max]}  вычитаем min = {arr[min]} = получаем разница между max min {arr[max] - arr[min]} " );
+// }
+
+// void FillArray(double[] array, double startNumber = 0, double finishNumber = 0)
+// {
+//     finishNumber++;
+//     Random random = new Random();
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = random.NextDouble();                //startNumber, finishNumber
+//     }
+
+//     Console.WriteLine();
+// }
+
+
+// void PrintArray(double[] array)
+// {
+//     Console.Write("Вывод массива");
+//     Console.WriteLine();
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// Zadacha38();
+
+
+
 
 void Zadacha38()
-{     
-    Random random = new Random();
-    int size = random.Next(4, 12);
-    double[] array = new double[size];
-    FillArray(array, -1000000000000000000, 10000000000000000000);
-    PrintArray(array);
-    DifferenceBetweenMaxAndMinArrayElements(array);
-    
-}
-
-
-void DifferenceBetweenMaxAndMinArrayElements(double[] arr)
 {
-    int min = 0;
-    int max = 0;
-    for (int i = 0; i < arr.Length; i++)
-    {
-        if (arr[i] > arr[max]) max = i;     
-        if (arr[i] < arr[min]) min = i;
-        
-    }
-    Console.WriteLine($"от max = {arr[max]}  вычитаем min = {arr[min]} = получаем разница между max min {arr[max] - arr[min]} " );
-}
-
-void FillArray(double[] array, double startNumber = 0, double finishNumber = 0)
-{
-    finishNumber++;
     Random random = new Random();
+    double[] array = new double[10];
+
     for (int i = 0; i < array.Length; i++)
     {
-        array[i] = random.NextDouble();                //startNumber, finishNumber
+        array[i] = Math.Round(random.NextDouble() * 10 - 5, 2);
     }
-    
-    Console.WriteLine();
-}
 
-
-void PrintArray(double[] array)
-{
     Console.Write("Вывод массива");
     Console.WriteLine();
     for (int i = 0; i < array.Length; i++)
     {
-        Console.Write(array[i] + " ");
+        Console.Write(array[i] + "\t");
+        //Console.Write(Math.Round(array[i], 2) + "\t");
     }
     Console.WriteLine();
+
+    double max = array[0];
+    double min = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] > max)
+            max = array[i];
+        if (array[i] < min)
+            min = array[i];
+    }
+    Console.WriteLine("Максимальное значение равно:" + max);
+    Console.WriteLine("Минимальное значение равно:" + min);
+    Console.WriteLine("их разность равна:" + (max - min));
 }
 
-Zadacha38();
+//Zadacha38();
+
+
+
+//  Рекурсия
+
+void Recursion(int n)
+{
+    if (n < 0)
+        return;
+    Console.WriteLine(n);
+    n--;
+    Recursion(n);
+    Console.WriteLine("Это рекурсия");
+}
+Recursion(1000);
