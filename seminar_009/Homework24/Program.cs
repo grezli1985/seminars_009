7d2ea9d17d86476a593a767f028b0cb05d9f0f2c
grezli <grezli@mail.ru>
2022-07-25 17:52:43 +0300

    add seminar 009 Homework 24

---
+++ seminar_009/Homework24/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 64:
+Задайте значения M и N. Напишите программу,
+которая выведет все натуральные числа в промежутке от M до N, кратные трём. */
+
+void Zadacha64()
+{
+    Console.Write("Видите число M: ");
+    int M = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Видите число N: ");
+    int N = Convert.ToInt32(Console.ReadLine());
+    MultiplesOfThree(M, N);
+}
+
+void MultiplesOfThree(int m, int n)
+{
+    if(m > n) return;
+    if(m % 3 == 0)
+    Console.Write(m + "\t");
+    m++;
+    MultiplesOfThree(m, n);
+}
+Console.WriteLine();
+
+
+Zadacha64();
